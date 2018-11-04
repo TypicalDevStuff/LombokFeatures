1c264eb3a98117c87d43e84c3dd59c9dbcccfa7c
Typical Dev <contact@typicaldev.com>
2018-11-04 18:22:19 +0100

    Initialize project and add Bicycle class with some Lombok annotations

---
+++ src/main/java/Bicycle.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bicycle {
+
+    private UUID id;
+
+    private String name;
+
+    private BigDecimal price;
+}
